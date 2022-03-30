ae73b70b1f60eefecbc562a1f623cef9434f4481
annaliliandersen <anna.l.l.andersen@ntnu.no>
2022-03-30 11:23:43 +0200

    tester tull

--- iris_anna.py
+++ iris_anna.py
@@ -2,8 +2,13 @@ import numpy as np
 from sklearn.datasets import load_iris
 
 
-#Prøver ny metode
-a = 1
-print(a)
+#data = load_iris()              #load_iris() is a link that provides documentation
+iris = load_iris()
 
-b = 2
+print('The data matrix:\n',iris['data'])
+print('The classification target:\n',iris['target'])
+print('The names of the dataset columns:\n',iris['feature_names'])
+print('The names of target classes:\n',iris['target_names'])
+
+
+annatull = 1
