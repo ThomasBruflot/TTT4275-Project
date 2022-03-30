3a9078390369e09ac6591e96c2e14cea7029202b
thomasbruflot <thomasbruflot@gmail.com>
2022-03-30 14:51:26 +0200

    Implementerte to funksjoner i iris.py fra kompendiet

--- iris.py
+++ iris.py
@@ -1,5 +1,6 @@
 import sklearn
 from sklearn.datasets import load_iris
+import numpy as np
 
 iris = load_iris()
 ##print(iris)
@@ -9,21 +10,29 @@ iris = load_iris()
 #print('The names of target classes:\n',iris['target_names'])
 #print('The full description of the dataset:\n',iris['DESCR'])
 #print('The path to the location of the data:\n',iris['filename'])
-''
+
 
 
 trainingSetSetosa = iris['data'][0:30]
-print(len(trainingSetSetosa))
 testingSetSetosa = iris['data'][30:50]
-print(len(testingSetSetosa))
-
 trainingSetVersicolor = iris['data'][50:80]
-print(len(trainingSetVersicolor))
 testingSetVersicolor = iris['data'][80:100]
-print(len(testingSetVersicolor))
-
 trainingSetVirginica = iris['data'][100:130]
-print(len(trainingSetVirginica))
 testingSetVirginica = iris['data'][130:150]
 
-print(len(testingSetVirginica))
+#Chapter 3.2 describes the MSE as a way to distinguish between the different iris types as it
+#is a linear seperable problem
+
+# g is a vector containing predicted labels
+# t is a vector containing true labels
+def calculate_MSE(g,t):
+    error = g-t
+    error_transposed = np.transpose(g-t)
+    mseVal = 1/2*np.sum(np.matmul(error,error_transposed)) 
+    return mseVal
+
+# Here x is a single input 
+# W is a CxD matrix where C is number of different classes 
+def calculate_prediction_g(x,W):
+    z_ik = np.matmul(x,W)
+    g = 1 / (1+ np.array(np.exp(-z_ik)))
