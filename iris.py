3b550deefd531cfaff16f62f67df8822d5a04766
thomasbruflot <thomasbruflot@gmail.com>
2022-04-02 15:02:19 +0200

    Implementerte noen funksjoner og jobbet litt med treningsalgoritmen

--- iris.py
+++ iris.py
@@ -2,6 +2,9 @@ import sklearn
 from sklearn.datasets import load_iris
 import numpy as np
 
+
+Num_Classes = 3
+Num_Features = 4
 iris = load_iris()
 ##print(iris)
 #print('The data matrix:\n',iris['data'])
@@ -18,7 +21,7 @@ trainingSetVirginica = iris['data'][100:130]
 testingSetVirginica = iris['data'][130:150]
 #The total training set is now a 90x4 (Rows x Columns) matrix
 totalTrainingSet = np.concatenate((trainingSetSetosa,trainingSetVersicolor,trainingSetVirginica), axis=0)
-print(totalTrainingSet)
+totalTrainingSet = np.reshape(totalTrainingSet,[30*Num_Classes,Num_Features])
 #We need a total array of t values, we thus need to make a function that makes an array containing 
 #the targets in iris set.
 def get_Targets():
@@ -52,24 +55,30 @@ def calculate_MSE(g,t):
 # Here x is a single input 
 # W is a CxD matrix where C is number of different classes 
 #Comp. eq. (20)
+def sigmoid(x):
+    return np.array(1/(1+np.exp(-x)))
+
 def calculate_prediction_g(x,W):
-    expList = []
-    #Loop through the samples and multiply with the W weights and use sigmoid to
-    #get predictions between 0 and 1
-    for i in range(len(x)): 
-        expList.append(np.exp(-(np.matmul(x[i],W))))
-    z_ikExponential = np.array(expList)
-    g = 1 / (1+ z_ikExponential)
+    g = np.zeros([30*Num_Classes,Num_Classes])
+    for i,sample in enumerate(x):
+        sample = np.append([sample],[1])
+        z = np.matmul(W,sample)
+        g[i] = sigmoid(z)
     return g
 
 #Need to implement equation (22) and (23)
 #Comp. Eq. (22):
 def calculate_gradW_MSE(g,t,x):
-    error = g-t
-    tmp = 1-g
-    tmp2 = np.multiply(error,tmp)
-    tmp3 = np.multiply(tmp2,g)
-    gradW_MSE = np.sum(np.matmul(tmp3,np.transpose(x)))
+    gradW_MSE = np.zeros([Num_Classes,Num_Features+1])
+    for xk,gk,tk in zip(x,g,t):    
+        xk = np.append([xk],[1]) #Lager basically en ny matrise med enere i siste del, 3x4->3x5 med 1 på slutten
+        xk = xk.reshape(Num_Features+1,1) #Her blir så dette transponert
+        #Under her implementeres grad_gk_MSE og så grad_zk_g 
+        grad_zk_g = (np.ones((Num_Classes,1))-gk.reshape(Num_Classes,1)) * (gk.reshape(Num_Classes,1))
+        grad_gk_MSE = ((gk-tk)).reshape(Num_Classes,1)
+        tmp = grad_gk_MSE * grad_zk_g
+        #Tar her og regner ut 
+        gradW_MSE += np.matmul(tmp, xk.reshape(1,Num_Features+1))
     return gradW_MSE
 
 #Comp. Eq. (23):
@@ -82,7 +91,7 @@ alpha = 0.5
 #x are the different training sets / samples
 
 def training_lin_classifier(trainingSetSamples,trainingSetTrueLabels,alpha, iterations=500):
-    W = np.zeros((3,5)) #number of classes and number of features as it is CxD and D is dimension for features, have 5 because of w_0
+    W = np.zeros((Num_Classes,Num_Features+1)) #number of classes and number of features as it is CxD and D is dimension for features, have 5 because of w_0
     MSE_List = []
     #Here we start the actual training by iterating through the training set and using the MSE
     for i in range(iterations):
