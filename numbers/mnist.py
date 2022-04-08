d682a84df6791d4c3f55c1dac1362f54d5c24d97
annaliliandersen <anna.l.l.andersen@ntnu.no>
2022-04-08 14:00:44 +0200

    test,train,euclidean

--- numbers/mnist.py
+++ numbers/mnist.py
@@ -1,2 +1,43 @@
-from keras.datasets import mnist
-print("hei")
+#https://pyimagesearch.com/2019/10/21/keras-vs-tf-keras-whats-the-difference-in-tensorflow-2-0/
+
+
+#--- God motivasjonsvideo
+#https://www.youtube.com/watch?v=aircAruvnKk
+
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.datasets import mnist
+from sklearn.cluster import KMeans
+
+#------------
+
+(train_x, train_y), (test_x, test_y) = mnist.load_data()
+
+print((train_x, train_y))
+print('x_train: ' + str(train_x.shape))
+print('y_train: ' + str(train_y.shape))
+print('x_test:  '  + str(test_x.shape))
+print('y_test:  '  + str(test_y.shape))
+#------------
+
+
+a =np.array((1,2,3))
+b=np.array((4,5,6))
+
+def calculate_euclidean_distance1(input_1,input_2, length):
+    distance = 0
+    for i in range(length-1):
+        distance += (input_1[i]-input_2[i])**2
+        Euclidean_distance = math.sqrt(distance)
+    return Euclidean_distance
+
+def calculate_euclidean_distance2(input_1,input_2):
+    Euclidean_distance = np.linalg.norm(input_1 - input_2)
+    print(Euclidean_distance)
+    return Euclidean_distance
+
+print("test Euclidean dist: ")
+calculate_euclidean_distance2(a,b) 
