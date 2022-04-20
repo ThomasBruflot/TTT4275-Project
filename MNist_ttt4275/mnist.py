4004e5751305d4083283dca344d9de4f6ee5d1b7
thomasbruflot <thomasbruflot@gmail.com>
2022-04-20 09:33:25 +0200

    endringer

--- MNist_ttt4275/mnist.py
+++ MNist_ttt4275/mnist.py
@@ -9,13 +9,15 @@ from sklearn.cluster import KMeans
 
 #------------
 
-(train_x, train_y), (test_x, test_y) = mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-print((train_x, train_y))
-print('x_train: ' + str(train_x.shape))
-print('y_train: ' + str(train_y.shape))
-print('x_test:  '  + str(test_x.shape))
-print('y_test:  '  + str(test_y.shape))
+#print((train_x, train_y))
+#print('x_train: ' + str(train_x.shape))
+#print('x_train_0: ' + str(train_x[0]))
+#print('y_train: ' + str(train_y.shape))
+print('y_train_5: ' + str(train_labels[5]))
+#print('x_test:  '  + str(test_x.shape))
+#print('y_test:  '  + str(test_y.shape))
 #------------
 
 
@@ -31,7 +33,7 @@ def calculate_euclidean_distance1(input_1,input_2, length):
 
 def calculate_euclidean_distance2(input_1,input_2):
     Euclidean_distance = np.linalg.norm(input_1 - input_2)
-    print(Euclidean_distance)
+    #print(Euclidean_distance)
     return Euclidean_distance
 
 print("test Euclidean dist: ")
@@ -50,13 +52,13 @@ def calculate_nearest_neighbour(trainImages, trainLabels, testImages, testLabels
     for i in range(NumTest):
         pred = 0 # New prediction for each testImage in the dataset
         indexPred = 0 #Index of prediction
-        minDiff = 0
+        minDist = 0
         for k in range(NumTrain):
             dist = calculate_euclidean_distance2(testImages[i],trainImages[k])
             if(k == 0):
-                minDiff = dist # We dont have a minimum difference first iteration
-            if(d < minDiff):
-                minDiff = dist
+                minDist = dist # We dont have a minimum difference first iteration
+            if(dist < minDist):
+                minDist = dist
                 indexPred = k #For each time we find a smaller difference we say ok this is our prediction
                 pred = trainLabels[k]
         if(pred == testLabels[i]):
@@ -66,8 +68,18 @@ def calculate_nearest_neighbour(trainImages, trainLabels, testImages, testLabels
             wrongPred.append([i,indexPred])
     return corrPred,wrongPred
 
+corrPred, wrongPred = calculate_nearest_neighbour(train_images,train_labels,test_images,test_labels,60000,10000)
 
-calculate_nearest_neighbour():
+testVal = 1
+print("CorrPred0: ", corrPred[testVal])
+print("prediction values: ", test_labels[corrPred[testVal][0]], " ", train_labels[corrPred[testVal][1]])
+print("prediction values: ", test_labels[1], " ", train_labels[360])
+print("All wrong predictions: ", str(len(wrongPred)))
+print("All correct predictions: ", str(len(corrPred)))
+
+
+
+
 
 
 
